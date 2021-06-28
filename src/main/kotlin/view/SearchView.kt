947bd09662060c26764fc5415b9fe35a7a725954
smoothbear <kjbin0420@naver.com>
2021-06-28 14:15:48 +0900

    Patch match message layout

--- src/main/kotlin/view/SearchView.kt
+++ src/main/kotlin/view/SearchView.kt
@@ -1,15 +1,15 @@
 package view
 
-import com.slack.api.model.block.Blocks.asBlocks
 import com.slack.api.model.block.Blocks.section
 import com.slack.api.model.block.LayoutBlock
 import com.slack.api.model.block.composition.BlockCompositions.markdownText
 import util.api.dto.MatchHistoryResult
 
 class SearchView {
-    fun buildMatchHistories(matchHistories: List<MatchHistoryResult>): MutableList<LayoutBlock> = asBlocks (
-        section { section -> section
-            .text(markdownText("게임 시간: ${matchHistories[0].info.gameDateTime}, 게임 정보: ${matchHistories[0].info.participants[0].level}"))
+    fun buildMatchHistories(matchHistories: List<MatchHistoryResult>): List<LayoutBlock> =
+        matchHistories.map { match ->
+            section { section -> section
+                .text(markdownText("게임한 날짜: ${match.info.gameDateTime}, 게임한 시간: ${match.info.gameLength}"))
+            }
         }
-    )
 }
